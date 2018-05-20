d5b6070a5996643fb3e225e247828e7ea49a7bff
kkkkoyo <mmkrz.sg@gmail.com>
2018-05-20 13:43:12 +0900

    ワイヤーを追加

--- ar-makevideo/ViewController.swift
+++ ar-makevideo/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+        //CreateSwitch()
     }
     //touches
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -35,10 +35,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                                           pointTransform.m42,pointTransform.m43)
         createBall(pointVector)
     }
+    var isWire = true
+
     //createBall
     func createBall(_ position: SCNVector3){
-        
-        let colors = [UIColor.white,UIColor.white,UIColor.white,UIColor.darkGray]
+
+        let colors = [UIColor.white]
         let size:CGFloat = 0.5
         let size_z:CGFloat = 0.01
 
@@ -48,18 +50,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let pattern = [sphere,sphere,sphere,pyramid,sphere,cube]
         
-        pattern[Int(arc4random_uniform(6))].firstMaterial?.diffuse.contents =
-        colors[Int(arc4random_uniform(4))]
+        pattern[Int(arc4random_uniform(UInt32(pattern.count)))].firstMaterial?.diffuse.contents =
+            colors[Int(arc4random_uniform(UInt32(colors.count)))]
         let node = SCNNode(geometry: pattern[Int(arc4random_uniform(6))])
 
         //node.position = SCNVector3Make(position.x,position.y-0.1,position.z)
         node.position = position
+        isWire = randomBoolWithYesPercent(percent: 50)
+        
+        if(isWire){
+            let node_opacity = [0.1 , 0.5, 1.0]
+            sceneView.debugOptions = [.showWireframe]
+            let num:Int = Int(arc4random_uniform(UInt32(node_opacity.count)))
+            node.opacity = CGFloat(node_opacity[num])
+        }else{
+            sceneView.debugOptions = []
+        }
+        
         print(node.position)
         if let camera = sceneView.pointOfView {
             //node.position = camera.convertPosition(position, to: nil)// カメラ位置からの偏差で求めた位置
-            print("2")
-            print(node.position)
-
             //node.eulerAngles = camera.eulerAngles  // カメラのオイラー角と同じにする
         }
         sceneView.scene.rootNode.addChildNode(node)
@@ -68,7 +78,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.position.z = -3
         node.opacity = 0
         node.rotation = SCNVector4(0, 0, 1, 5)
-
+    }
+    func CreateSwitch(){
+        // Swicthを作成する.
+        let mySwicth: UISwitch = UISwitch()
+        mySwicth.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height - 200)
+        // Swicthの枠線を表示する.
+        mySwicth.tintColor = UIColor.black
+        // SwitchをOnに設定する.
+        mySwicth.isOn = true
+        // SwitchのOn/Off切り替わりの際に、呼ばれるイベントを設定する.
+        mySwicth.addTarget(self, action: #selector(ViewController.onClickMySwicth(sender:)), for: UIControlEvents.valueChanged)
+        // SwitchをViewに追加する.
+        self.view.addSubview(mySwicth)
+    }
+    @objc internal func onClickMySwicth(sender: UISwitch){
+        if sender.isOn {
+            print("on")
+            isWire = true
+        }
+        else {
+            print("off")
+            isWire = false
+        }
+    }
+    func randomBoolWithYesPercent(percent: UInt32) -> Bool {
+        return arc4random_uniform(100) < percent
     }
     
     override func viewWillAppear(_ animated: Bool) {
